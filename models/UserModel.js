d8b0f5db1b4684952cf59a9714974e5390486fe9
jimish1806 <jimish957@gmail.com>
2023-07-12 11:57:10 +0530

    initial commit

---
+++ models/UserModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema(
+    {
+        name :
+        {
+            type : String,
+            required : true,
+            trim : true
+        },
+        email :
+        {
+            type : String,
+            required : true,
+            unique : true
+        },
+        password : 
+        {
+            type : String,
+            required : true
+        },
+        phone :
+        {
+            type :  String,
+            required : true
+        },
+        address :
+        {
+            type : String,
+            required : true
+        },
+        role : 
+        {
+            type : Number,
+            default : 0
+        }
+    }, {timestamps : true}
+)
+
+const User = mongoose.model('users',UserSchema);
+
+module.exports = User;
