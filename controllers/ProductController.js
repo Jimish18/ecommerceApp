606cb33c6acf71c7b6b6c36b245b7905daa1eba6
jimish1806 <jimish957@gmail.com>
2023-07-15 23:49:36 +0530

    creating product endpoints on backend

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,8 +1,47 @@
+const  slugify  = require('slugify');
+const ProductModel = require('../models/ProductModel');
+const fs = require('fs');
+
 const createProductController = async (req,res) =>
 {
     try 
     {
-        // const     
+        const {name ,  description , price , category , quantity , shipping} = req.fields ; 
+        const {photo} = req.files;
+
+        switch (true) {
+            case !name:
+                return res.status(500).json({error : 'Name is Required'});
+            case !description:
+                return res.status(500).json({error : 'description is Required'});
+            case !price:
+                return res.status(500).json({error : 'price is Required'});    
+            case !category:
+                return res.status(500).json({error : 'category is Required'});
+            case !quantity:
+                return res.status(500).json({error : 'quantity is Required'});
+            case !photo || photo.size > 1000000:
+                return res.status(500).json({error : 'photo is Required and size should be less than 1mb'});       
+            
+        }
+
+        const product = new ProductModel({...req.fields , slug : slugify(name)});
+        
+        if(photo)
+        {
+            product.photo.data = fs.readFileSync(photo.path);
+            product.photo.contentType = photo.type
+        }
+
+        await product.save();
+
+        res.status(201).json(
+            {
+                success : true,
+                message : "Product created successfully",
+                product
+            }
+        )
     } 
     catch (error) 
     {
@@ -17,5 +56,9 @@ const createProductController = async (req,res) =>
     }
 };
 
+const getProductController = async (req,res) =>
+{
 
-module.exports = {createProductController};
+}
+
+module.exports = {createProductController , getProductController};
--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { requireSignIn, isAdmin } = require('../middlewares/AuthMiddleware');
-const {createProductController} = require('../controllers/ProductController');
+const {createProductController , getProductController} = require('../controllers/ProductController');
+const formidable = require('express-formidable');
 
-router.post('/create-product' , requireSignIn , isAdmin , createProductController);
+router.post('/create-product' , requireSignIn , isAdmin , formidable(), createProductController);
+
+router.post('/get-product' ,  getProductController);
 
 
 module.exports = router;
